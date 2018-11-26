4be6b6fbc420631ebb0aaa3fed0f7b00b9f4be0f
Xiaoqi Wei <xiaoqiwei@xiaoqideimac.local>
2018-11-26 13:22:48 +0800

    Initial Commit

---
+++ AutolayoutThree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutThree
+//
+//  Created by Xiaoqi Wei on 2018/11/26.
+//  Copyright © 2018年 Xiaoqi Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
